97c9ee3a7272ed9795250cfe6276a981f7558a0d
KiarashPahlavanian <kiarash.1379.pahlavanian@gmail.com>
2024-06-22 10:57:15 +0330

    Update Herami.py

--- Herami.py
+++ Herami.py
@@ -1,4 +1,4 @@
-def heapify(arr, n, i):
+def TabeHerami(arr, n, i):
     BozorgTarin = i  
     chap = 2 * i + 1  
     rast = 2 * i + 2  
@@ -16,21 +16,21 @@ def heapify(arr, n, i):
         arr[i], arr[BozorgTarin] = arr[BozorgTarin], arr[i]  
 
         
-        heapify(arr, n, BozorgTarin)
+        TabeHerami(arr, n, BozorgTarin)
 
-def heap_sort(arr):
+def Herami(arr):
     n = len(arr)
 
     
     for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
+        TabeHerami(arr, n, i)
 
     
     for i in range(n-1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i]  
-        heapify(arr, i, 0)
+        TabeHerami(arr, i, 0)
 
 
 data = [12, 11, 13, 5, 6, 7]
-heap_sort(data)
-print("Sorted array is:", data)
+Herami(data)
+print("Araye sort shode:", data)
